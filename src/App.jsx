9b40b50a67dfebf3fa58cb6f4a5e1835d273ac6e
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-30 17:01:23 -0500

    feat: add Header and Footer components to App and refactor LastArticle styles
    This commit introduces the Header and Footer components to the App layout, enhancing the overall structure of the application. Additionally, the LastArticle component's class names have been reordered for better readability and consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import MainPage from '@pages/MainPage'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
 function App() {
   return (
     <>
+    <Header />
     <MainPage />
+    <Footer />
     </>
   )
 }
