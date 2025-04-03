12ff24709d832cea8638c4542be06fd95c81a7a0
Mathias Rendon <mathiassaid7@gmail.com>
2025-04-03 18:25:41 -0500

    feat(routing): add dynamic routing with Outlet and conditional rendering
    Integrate react-router's Outlet for nested routing and conditionally render MainPage based on the current route. This change enables better routing flexibility and ensures the MainPage is only displayed on the root path.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import MainPage from '@pages/MainPage'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { Outlet, useLocation } from 'react-router'
 
 function App() {
+  const location = useLocation()
+  const mainPage = location.pathname === '/'
+
+
   return (
     <>
     <Header />
-    <MainPage />
+    <Outlet />
+    {mainPage && <MainPage />}
     <Footer />
     </>
   )
