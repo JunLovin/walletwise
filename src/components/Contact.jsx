ee2b71834266647a57c4a652ed12a98153081d6c
Mathias Rendon <mathiassaid7@gmail.com>
2025-04-21 17:13:17 -0500

    refactor(ui): improve responsive design and add mobile navigation modal
    Updated the layout and styles in `index.html`, `Contact.jsx`, and `Header.jsx` to enhance responsiveness. Added a modal for mobile navigation in `Header.jsx` to improve user experience on smaller screens. Adjusted class names and styles to ensure consistency across devices.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,45 +1,39 @@
 function Contact() {
     return (
         <>
-        <div className="flex gap-4 justify-around items-center text-black transition-colors duration-300 contact-container dark:text-white min-h-[90dvh]">
-            <div className="contact-left w-[50%] flex flex-col gap-4 justify-around">
+        <div className="flex gap-4 justify-around items-center text-black transition-colors duration-300 contact-container dark:text-white min-h-[90dvh] max-md:flex-col max-md:gap-20 mb-4">
+            <div className="contact-left w-[50%] flex flex-col gap-4 justify-around max-md:text-center max-md:w-full">
                 <div className="contact-left-title">
-                    <h2 className="text-5xl font-bold leading-normal">¡Contáctame!</h2>
+                    <h2 className="text-5xl font-bold leading-normal max-xl:text-4xl">¡Contáctame!</h2>
                 </div>
                 <div className="contact-left-description">
-                    <p className="max-w-[70ch] leading-8">¿Listo para transformar tus finanzas? Estoy aquí para ayudarte a alcanzar tus metas financieras. Ya sea que necesites asesoría personalizada o quieras discutir cómo WalletWise puede adaptarse a tu negocio, ¡conversemos! Tu éxito financiero comienza con una simple conversación.</p>
+                    <p className="max-w-[60ch] leading-8 max-md:max-w-full">¿Listo para transformar tus finanzas? Estoy aquí para ayudarte a alcanzar tus metas financieras. Ya sea que necesites asesoría personalizada o quieras discutir cómo WalletWise puede adaptarse a tu negocio, ¡conversemos! Tu éxito financiero comienza con una simple conversación.</p>
                 </div>
                 <div className="mt-4 contact-left-creator">
                     <p className="text-xl font-semibold">- Said ruiz</p>
                 </div>
-                <div className="mt-8 contact-left-button">
+                <div className="mt-8 max-md:mt-4 contact-left-button">
                     <button onClick={() => {
                         window.open('https://github.com/Junlovin', '_blank')
-                    }} className="w-40 h-12 font-semibold text-yellow-400 rounded-full border-2 border-yellow-300 shadow-2xl transition-colors duration-300 cursor-pointer dark:hover:border-yellow-500 dark:active:bg-yellow-600 dark:hover:bg-yellow-500 hover:bg-yellow-400 hover:text-white active:bg-yellow-500 dark:shadow-yellow-800 dark:shadow-xl">GitHub</button>
+                    }} className="w-40 h-12 font-semibold text-yellow-400 rounded-full border-2 border-yellow-300 shadow-2xl transition-colors duration-300 cursor-pointer max-md:w-[90%] dark:hover:border-yellow-500 dark:active:bg-yellow-600 dark:hover:bg-yellow-500 hover:bg-yellow-400 hover:text-white active:bg-yellow-500 dark:shadow-yellow-800 dark:shadow-xl">GitHub</button>
                 </div>
             </div>
             <div className="contact-right w-[50%]">
                 <form onSubmit={(e) => e.preventDefault()} className="flex flex-col gap-8 items-center">
-                    <div className="flex gap-4 justify-around nombreYApellido">
-                        <div className="flex flex-col gap-2 nombre">
-                            <label htmlFor="name" className="font-semibold">Nombre</label>
-                            <input type="text" id="name" name="name" className="w-[200px] h-[40px] rounded-xl border-black border-2 px-4 dark:border-neutral-300"/>
-                        </div>
-                        <div className="flex flex-col gap-2 apellido">
-                            <label htmlFor="lastname" className="font-semibold">Apellido</label>
-                            <input type="text" id="lastname" name="lastname" className="w-[200px] h-[40px] rounded-xl border-black border-2 px-4 dark:border-neutral-300" />
-                        </div>
+                    <div className="flex flex-col gap-2 w-[70%] max-md:w-full nombre">
+                        <label htmlFor="name" className="font-semibold">Nombre</label>
+                        <input type="text" id="name" name="name" className="w-full px-4 h-[40px] rounded-xl border-black border-2 dark:border-neutral-300"/>
                     </div>
-                    <div className="flex flex-col gap-2 email">
+                    <div className="flex flex-col gap-2 email w-[70%] max-md:w-full">
                         <label htmlFor="email" className="font-semibold">Correo</label>
-                        <input type="text" id="email" name="email" className="w-[410px] h-[40px] rounded-xl border-black border-2 px-4 dark:border-neutral-300"/>
+                        <input type="text" id="email" name="email" className="w-full h-[40px] rounded-xl border-black border-2 px-4 dark:border-neutral-300"/>
                     </div>
-                    <div className="flex flex-col gap-2 message">
+                    <div className="flex flex-col gap-2 message w-[70%] max-md:w-full">
                         <label htmlFor="message" className="font-semibold">Message</label>
-                        <textarea name="message" id="message" className="w-[410px] h-[125px] border-2 border-text rounded-xl resize-none px-2 py-2"></textarea>
+                        <textarea name="message" id="message" className="w-full h-[125px] border-2 border-text rounded-xl resize-none px-2 py-2"></textarea>
                     </div>
-                    <div className="flex justify-center items-center submit-button">
-                        <button className="w-32 h-12 font-semibold text-white bg-yellow-400 rounded-full border-white shadow-2xl transition-colors duration-300 cursor-pointer hover:border-2 hover:bg-transparent dark:bg-yellow-500 dark:hover:bg-transparent dark:hover:border-yellow-500 hover:border-yellow-400 hover:text-yellow-400 active:bg-neutral-200 dark:shadow-yellow-800 dark:shadow-xl">Enviar</button>
+                    <div className="flex justify-center items-center w-full submit-button">
+                        <button className="w-32 h-12 max-md:w-[90%] font-semibold text-white bg-yellow-400 rounded-full border-white shadow-2xl transition-colors duration-300 cursor-pointer hover:border-2 hover:bg-transparent dark:bg-yellow-500 dark:hover:bg-transparent dark:hover:border-yellow-500 hover:border-yellow-400 hover:text-yellow-400 active:bg-neutral-200 dark:shadow-yellow-800 dark:shadow-xl">Enviar</button>
                     </div>
                 </form>
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { Link } from "react-router"
+import ReactModal from 'react-modal'
 
 function Header() {
+    const [openNavBar, setOpenNavBar] = useState(false)
     const [darkMode, setDarkMode] = useState(false)
 
-    const handleDarkMode =  () => {
+    const handleDarkMode = () => {
         let darkMode = document.querySelector('html')
         darkMode.classList.toggle('dark')
         if (darkMode.classList.contains('dark')) {
@@ -15,33 +17,110 @@ function Header() {
         console.log(darkMode)
     }
 
+    const handleModal = () => {
+        setOpenNavBar(!openNavBar)
+    }
+
     return (
         <>
-        <header className="w-full h-16.5 flex justify-around items-center text-[var(--dark-blue-primary)] dark:text-[var(--light-gray-dark-mode)] transition-colors duration-300">
-            <Link to="/">
-            <div className="flex items-center w-max header-left h-max">
-                <svg xmlns="http://www.w3.org/2000/svg" className="text-[var(--yellow-gold)] w-[54px] h-[46px]" width="1024" height="1024" viewBox="0 0 1024 1024"><path fill="currentColor" d="M256 640v192h640V384H768v-64h150.976c14.272 0 19.456 1.472 24.64 4.288a29.06 29.06 0 0 1 12.16 12.096c2.752 5.184 4.224 10.368 4.224 24.64v493.952c0 14.272-1.472 19.456-4.288 24.64a29.06 29.06 0 0 1-12.096 12.16c-5.184 2.752-10.368 4.224-24.64 4.224H233.024c-14.272 0-19.456-1.472-24.64-4.288a29.06 29.06 0 0 1-12.16-12.096c-2.688-5.184-4.224-10.368-4.224-24.576V640z"/><path fill="currentColor" d="M768 192H128v448h640zm64-22.976v493.952c0 14.272-1.472 19.456-4.288 24.64a29.06 29.06 0 0 1-12.096 12.16c-5.184 2.752-10.368 4.224-24.64 4.224H105.024c-14.272 0-19.456-1.472-24.64-4.288a29.06 29.06 0 0 1-12.16-12.096C65.536 682.432 64 677.248 64 663.04V169.024c0-14.272 1.472-19.456 4.288-24.64a29.06 29.06 0 0 1 12.096-12.16C85.568 129.536 90.752 128 104.96 128h685.952c14.272 0 19.456 1.472 24.64 4.288a29.06 29.06 0 0 1 12.16 12.096c2.752 5.184 4.224 10.368 4.224 24.64z"/><path fill="currentColor" d="M448 576a160 160 0 1 1 0-320a160 160 0 0 1 0 320m0-64a96 96 0 1 0 0-192a96 96 0 0 0 0 192"/></svg>
-                <h1 className="text-4xl">WalletWise</h1>
-            </div>
-            </Link>
-            <div className="header-right">
-                <ul className="flex items-center">
-                    <li className="navBar"><Link to="/">Inicio</Link></li>
-                    <li className="navBar"><Link to="/">Artículos</Link></li>
-                    <li className="navBar"><Link to="/">Categorías</Link></li>
-                    <li className="navBar"><Link to="/">Sobre Mí</Link></li>
-                    <li className="navBar"><Link to="/contacto">Contacto</Link></li>
-                    <li className="cursor-pointer" onClick={handleDarkMode}>                
-                    { !darkMode ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" className="max-[30rem]:hidden" fillRule="evenodd" d="M11.017 2.802a9.25 9.25 0 1 0 10.181 10.181A7.25 7.25 0 1 1 11.017 2.802M1.25 12C1.25 6.063 6.063 1.25 12 1.25c.717 0 1.075.571 1.137 1.026c.059.438-.103.995-.606 1.299a5.75 5.75 0 1 0 7.894 7.894c.304-.503.861-.665 1.299-.606c.455.062 1.026.42 1.026 1.137c0 5.937-4.813 10.75-10.75 10.75S1.25 17.937 1.25 12" clipRule="evenodd"/></svg>
+            <header className="w-full h-16.5 flex justify-around items-center text-[var(--dark-blue-primary)] dark:text-[var(--light-gray-dark-mode)] transition-colors duration-300 mb-8">
+                <Link to="/">
+                    <div className="flex items-center w-max header-left h-max">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="text-[var(--yellow-gold)] w-[54px] h-[46px]" width="1024" height="1024" viewBox="0 0 1024 1024"><path fill="currentColor" d="M256 640v192h640V384H768v-64h150.976c14.272 0 19.456 1.472 24.64 4.288a29.06 29.06 0 0 1 12.16 12.096c2.752 5.184 4.224 10.368 4.224 24.64v493.952c0 14.272-1.472 19.456-4.288 24.64a29.06 29.06 0 0 1-12.096 12.16c-5.184 2.752-10.368 4.224-24.64 4.224H233.024c-14.272 0-19.456-1.472-24.64-4.288a29.06 29.06 0 0 1-12.16-12.096c-2.688-5.184-4.224-10.368-4.224-24.576V640z" /><path fill="currentColor" d="M768 192H128v448h640zm64-22.976v493.952c0 14.272-1.472 19.456-4.288 24.64a29.06 29.06 0 0 1-12.096 12.16c-5.184 2.752-10.368 4.224-24.64 4.224H105.024c-14.272 0-19.456-1.472-24.64-4.288a29.06 29.06 0 0 1-12.16-12.096C65.536 682.432 64 677.248 64 663.04V169.024c0-14.272 1.472-19.456 4.288-24.64a29.06 29.06 0 0 1 12.096-12.16C85.568 129.536 90.752 128 104.96 128h685.952c14.272 0 19.456 1.472 24.64 4.288a29.06 29.06 0 0 1 12.16 12.096c2.752 5.184 4.224 10.368 4.224 24.64z" /><path fill="currentColor" d="M448 576a160 160 0 1 1 0-320a160 160 0 0 1 0 320m0-64a96 96 0 1 0 0-192a96 96 0 0 0 0 192" /></svg>
+                        <h1 className="text-4xl max-lg:hidden">WalletWise</h1>
+                    </div>
+                </Link>
+                <div className="header-right">
+                    <ul className="flex items-center">
+                        <li className="navBar max-md:hidden"><Link to="/">Inicio</Link></li>
+                        <li className="navBar max-md:hidden"><Link to="/">Artículos</Link></li>
+                        <li className="navBar max-md:hidden"><Link to="/">Categorías</Link></li>
+                        <li className="navBar max-md:hidden"><Link to="/">Sobre Mí</Link></li>
+                        <li className="navBar max-md:hidden"><Link to="/contacto">Contacto</Link></li>
+                        <li className="cursor-pointer" onClick={handleDarkMode}>
+                            {!darkMode ? (
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" className="max-md:hidden" fillRule="evenodd" d="M11.017 2.802a9.25 9.25 0 1 0 10.181 10.181A7.25 7.25 0 1 1 11.017 2.802M1.25 12C1.25 6.063 6.063 1.25 12 1.25c.717 0 1.075.571 1.137 1.026c.059.438-.103.995-.606 1.299a5.75 5.75 0 1 0 7.894 7.894c.304-.503.861-.665 1.299-.606c.455.062 1.026.42 1.026 1.137c0 5.937-4.813 10.75-10.75 10.75S1.25 17.937 1.25 12" clipRule="evenodd" /></svg>
+                            ) : (
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" className="max-md:hidden" d="m5.64 17l-.71.71a1 1 0 0 0 0 1.41a1 1 0 0 0 1.41 0l.71-.71A1 1 0 0 0 5.64 17M5 12a1 1 0 0 0-1-1H3a1 1 0 0 0 0 2h1a1 1 0 0 0 1-1m7-7a1 1 0 0 0 1-1V3a1 1 0 0 0-2 0v1a1 1 0 0 0 1 1M5.64 7.05a1 1 0 0 0 .7.29a1 1 0 0 0 .71-.29a1 1 0 0 0 0-1.41l-.71-.71a1 1 0 0 0-1.41 1.41Zm12 .29a1 1 0 0 0 .7-.29l.71-.71a1 1 0 1 0-1.41-1.41l-.64.71a1 1 0 0 0 0 1.41a1 1 0 0 0 .66.29ZM21 11h-1a1 1 0 0 0 0 2h1a1 1 0 0 0 0-2m-9 8a1 1 0 0 0-1 1v1a1 1 0 0 0 2 0v-1a1 1 0 0 0-1-1m6.36-2A1 1 0 0 0 17 18.36l.71.71a1 1 0 0 0 1.41 0a1 1 0 0 0 0-1.41ZM12 6.5a5.5 5.5 0 1 0 5.5 5.5A5.51 5.51 0 0 0 12 6.5m0 9a3.5 3.5 0 1 1 3.5-3.5a3.5 3.5 0 0 1-3.5 3.5" /></svg>
+                            )}
+                        </li>
+                        <svg xmlns="http://www.w3.org/2000/svg" onClick={handleModal} width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" className="hidden cursor-pointer max-md:block" strokeLinecap="round" strokeWidth="2" d="M5 7h14M5 12h14M5 17h14" /></svg>
+                    </ul>
+                </div>
+                <ReactModal
+                    isOpen={openNavBar}
+                    onRequestClose={handleModal}
+                    style={{
+                        overlay: {
+                            backgroundColor: 'rgba(0, 0, 0, 0.3)',
+                            backdropFilter: 'blur(4px)',
+                        },
+                        content: {
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between',
+                            backgroundColor: darkMode ? '#2C3E50' : '#ECF0F1',
+                            color: '#F39C12',
+                            position: 'fixed',
+                            left: 'auto',
+                            right: '0',
+                            top: '0',
+                            bottom: '0',
+                            width: '90%',
+                            maxWidth: '400px',
+                            height: '100%',
+                            padding: '20px',
+                            borderRadius: '0',
+                            transform: 'none',
+                            margin: '0',
+                            border: 'none',
+                            overflowX: 'scroll',
+                            transition: 'all 200ms linear'
+                        }
+                    }}
+    
+                >
+                    <svg onClick={() => {
+                        setOpenNavBar(false)
+                    }}  xmlns="http://www.w3.org/2000/svg"  width="28"  height="28"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="absolute top-4.5 right-4"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+                    <span onClick={handleDarkMode} className="w-max">
+                    {!darkMode ? (
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" className="" fillRule="evenodd" d="M11.017 2.802a9.25 9.25 0 1 0 10.181 10.181A7.25 7.25 0 1 1 11.017 2.802M1.25 12C1.25 6.063 6.063 1.25 12 1.25c.717 0 1.075.571 1.137 1.026c.059.438-.103.995-.606 1.299a5.75 5.75 0 1 0 7.894 7.894c.304-.503.861-.665 1.299-.606c.455.062 1.026.42 1.026 1.137c0 5.937-4.813 10.75-10.75 10.75S1.25 17.937 1.25 12" clipRule="evenodd" /></svg>
                     ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" className="max-[30rem]:hidden"  d="m5.64 17l-.71.71a1 1 0 0 0 0 1.41a1 1 0 0 0 1.41 0l.71-.71A1 1 0 0 0 5.64 17M5 12a1 1 0 0 0-1-1H3a1 1 0 0 0 0 2h1a1 1 0 0 0 1-1m7-7a1 1 0 0 0 1-1V3a1 1 0 0 0-2 0v1a1 1 0 0 0 1 1M5.64 7.05a1 1 0 0 0 .7.29a1 1 0 0 0 .71-.29a1 1 0 0 0 0-1.41l-.71-.71a1 1 0 0 0-1.41 1.41Zm12 .29a1 1 0 0 0 .7-.29l.71-.71a1 1 0 1 0-1.41-1.41l-.64.71a1 1 0 0 0 0 1.41a1 1 0 0 0 .66.29ZM21 11h-1a1 1 0 0 0 0 2h1a1 1 0 0 0 0-2m-9 8a1 1 0 0 0-1 1v1a1 1 0 0 0 2 0v-1a1 1 0 0 0-1-1m6.36-2A1 1 0 0 0 17 18.36l.71.71a1 1 0 0 0 1.41 0a1 1 0 0 0 0-1.41ZM12 6.5a5.5 5.5 0 1 0 5.5 5.5A5.51 5.51 0 0 0 12 6.5m0 9a3.5 3.5 0 1 1 3.5-3.5a3.5 3.5 0 0 1-3.5 3.5"/></svg>                
-                    ) }
-                    </li>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" className="hidden cursor-pointer max-sm:block" strokeLinecap="round" strokeWidth="2" d="M5 7h14M5 12h14M5 17h14"/></svg>
-                </ul>
-            </div>
-        </header>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" className="" d="m5.64 17l-.71.71a1 1 0 0 0 0 1.41a1 1 0 0 0 1.41 0l.71-.71A1 1 0 0 0 5.64 17M5 12a1 1 0 0 0-1-1H3a1 1 0 0 0 0 2h1a1 1 0 0 0 1-1m7-7a1 1 0 0 0 1-1V3a1 1 0 0 0-2 0v1a1 1 0 0 0 1 1M5.64 7.05a1 1 0 0 0 .7.29a1 1 0 0 0 .71-.29a1 1 0 0 0 0-1.41l-.71-.71a1 1 0 0 0-1.41 1.41Zm12 .29a1 1 0 0 0 .7-.29l.71-.71a1 1 0 1 0-1.41-1.41l-.64.71a1 1 0 0 0 0 1.41a1 1 0 0 0 .66.29ZM21 11h-1a1 1 0 0 0 0 2h1a1 1 0 0 0 0-2m-9 8a1 1 0 0 0-1 1v1a1 1 0 0 0 2 0v-1a1 1 0 0 0-1-1m6.36-2A1 1 0 0 0 17 18.36l.71.71a1 1 0 0 0 1.41 0a1 1 0 0 0 0-1.41ZM12 6.5a5.5 5.5 0 1 0 5.5 5.5A5.51 5.51 0 0 0 12 6.5m0 9a3.5 3.5 0 1 1 3.5-3.5a3.5 3.5 0 0 1-3.5 3.5" /></svg>
+                    )}
+                    </span>
+                    <div className="flex items-center w-full h-full links">
+                        <ul className="flex flex-col justify-around h-[60%]">
+                            <li className="flex gap-2 items-center">
+                                <svg  xmlns="http://www.w3.org/2000/svg"  width="32"  height="32"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
+                                <h2 className="text-xl text-[var(--light-gray)] font-semibold"><Link to="/">Inicio</Link></h2>
+                            </li>
+                            <li className="flex gap-2 items-center">
+                                <svg  xmlns="http://www.w3.org/2000/svg"  width="32"  height="32"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16 6h3a1 1 0 0 1 1 1v11a2 2 0 0 1 -4 0v-13a1 1 0 0 0 -1 -1h-10a1 1 0 0 0 -1 1v12a3 3 0 0 0 3 3h11" /><path d="M8 8l4 0" /><path d="M8 12l4 0" /><path d="M8 16l4 0" /></svg>
+                                <h2 className="text-xl text-[var(--light-gray)] font-semibold"><Link to="/articles">Artículos</Link></h2>
+                            </li>
+                            <li className="flex gap-2 items-center">
+                                <svg  xmlns="http://www.w3.org/2000/svg"  width="32"  height="32"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4h6v6h-6z" /><path d="M14 4h6v6h-6z" /><path d="M4 14h6v6h-6z" /><path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /></svg>
+                                <h2 className="text-xl text-[var(--light-gray)] font-semibold"><Link to="/categories">Categorías</Link></h2>
+                            </li>
+                            <li className="flex gap-2 items-center">
+                                <svg  xmlns="http://www.w3.org/2000/svg"  width="32"  height="32"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
+                                <h2 className="text-xl text-[var(--light-gray)] font-semibold"><Link to="/aboutme">Sobre mí</Link></h2>
+                            </li>
+                            <li className="flex gap-2 items-center">
+                                <svg  xmlns="http://www.w3.org/2000/svg"  width="32"  height="32"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round" ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" /></svg>
+                                <h2 className="text-xl text-[var(--light-gray)] font-semibold"><Link to="/contacto">Contacto</Link></h2>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="flex relative right-0 bottom-0 items-center w-full h-20 credits justify-end pr-4 text-[var(--yellow-gold)]">
+                        <svg onClick={() => {
+                            window.open('https://github.com/JunLovin', '_blank')
+                        }} xmlns="http://www.w3.org/2000/svg"  width="28"  height="28"  viewBox="0 0 24 24"  fill="currentColor"  className=""><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5.315 2.1c.791 -.113 1.9 .145 3.333 .966l.272 .161l.16 .1l.397 -.083a13.3 13.3 0 0 1 4.59 -.08l.456 .08l.396 .083l.161 -.1c1.385 -.84 2.487 -1.17 3.322 -1.148l.164 .008l.147 .017l.076 .014l.05 .011l.144 .047a1 1 0 0 1 .53 .514a5.2 5.2 0 0 1 .397 2.91l-.047 .267l-.046 .196l.123 .163c.574 .795 .93 1.728 1.03 2.707l.023 .295l.007 .272c0 3.855 -1.659 5.883 -4.644 6.68l-.245 .061l-.132 .029l.014 .161l.008 .157l.004 .365l-.002 .213l-.003 3.834a1 1 0 0 1 -.883 .993l-.117 .007h-6a1 1 0 0 1 -.993 -.883l-.007 -.117v-.734c-1.818 .26 -3.03 -.424 -4.11 -1.878l-.535 -.766c-.28 -.396 -.455 -.579 -.589 -.644l-.048 -.019a1 1 0 0 1 .564 -1.918c.642 .188 1.074 .568 1.57 1.239l.538 .769c.76 1.079 1.36 1.459 2.609 1.191l.001 -.678l-.018 -.168a5.03 5.03 0 0 1 -.021 -.824l.017 -.185l.019 -.12l-.108 -.024c-2.976 -.71 -4.703 -2.573 -4.875 -6.139l-.01 -.31l-.004 -.292a5.6 5.6 0 0 1 .908 -3.051l.152 -.222l.122 -.163l-.045 -.196a5.2 5.2 0 0 1 .145 -2.642l.1 -.282l.106 -.253a1 1 0 0 1 .529 -.514l.144 -.047l.154 -.03z" /></svg>
+                    </div>
+                </ReactModal>
+            </header>
         </>
     )
 }
