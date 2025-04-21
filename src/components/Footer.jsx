969e93ae0d254f3d42149390865e5d39f5981c3d
Mathias Rendon <mathiassaid7@gmail.com>
2025-04-21 17:13:32 -0500

    feat: add Footer component to MainPage
    Introduce a new Footer component and integrate it into the MainPage to display a consistent footer across the application. The footer includes attribution and a link to the author's GitHub profile.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="flex absolute bottom-2 justify-center items-center w-full text-[var(--light-gray)] font-semibold">
+            <span>Hecho por <a href="https://github.com/JunLovin" target="_blank" className="underline underline-offset-2">JunLovin</a> con ♥️ y ☕</span>
+        </footer>
+    )
+}
+
+export default Footer
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,11 @@
+import Footer from "../components/Footer"
+
 function MainPage() {
     return (
         <>
         <div className="px-12 mt-8 mb-20 w-full">
         </div>
+        <Footer />
         </>
     )
 }
